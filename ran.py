81c9ce32cfb98aa3c4c064a3a3bbf44795993984
neapolis123 <spirovsk@gmail.com>
2024-11-28 13:02:29 +0100

    Scraped Dates

---
+++ ran.py
@@ -0,0 +1,14 @@
+from seleniumbase import SB
+from bs4 import BeautifulSoup
+
+
+url ='https://finviz.com/quote.ashx?t=DRUG&p=d'
+with SB(uc=True) as sb:    
+    sb.activate_cdp_mode(url)
+    sb.sleep(2)
+    table = sb.cdp.find('table#news-table')
+    print('table')
+    print(table)
+    print(type(table))
+    soup = BeautifulSoup(table.get_html(),'html.parser')
+
